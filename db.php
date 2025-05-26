2a458445286ae05325747ec0e711cfd2c18bb307
ayalkadimail <al.kadiriaya.502@gmail.com>
2025-05-26 01:06:19 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "MyStrongP@ssword123", "qcm");
+$conn = new mysqli("localhost", "root", "", "qcm");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
